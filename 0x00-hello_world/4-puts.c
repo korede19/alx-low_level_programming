e26e271ab47966d2051ace617cb31ca7fb8333a9
korede19 <oyeyemikorede5@gmail.com>
2021-06-17 12:36:44 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
 * this program prints "Programming is like building a multilingual puzzle"
 * Return: o
 */
-init main(void)
+int main(void)
 {
  puts("\"Programming is like building a multilingual puzzle"):
  return (0)

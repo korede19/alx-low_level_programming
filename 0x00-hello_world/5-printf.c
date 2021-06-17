49a19e8ce806819a42b8b5bc4ab4b5684ad2d602
korede19 <oyeyemikorede5@gmail.com>
2021-06-17 13:21:48 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#iclude <stdio.h>
+
+/**
+* main
+* iprints info asshown below
+* return: always 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the  outcome is a piece of art,\n");
+return (0);
+}.

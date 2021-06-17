a7c856a7a0ca4d3c2a1402db6be37333f236650e
korede19 <oyeyemikorede5@gmail.com>
2021-06-17 14:03:39 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <sudio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "the info below"
+* Return: 0;
+*/
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));  
+printf("Size of size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n"
+, (unsigned long)sizeof(float));
+printf("Size of a float: %lu byte(s)\n"
+, (unsigned long)sizeof(float));
+return(0)
+}

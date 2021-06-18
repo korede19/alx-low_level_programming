8af4f74046e86899c6de33d53567e311f779f16b
root <oyeyemikorede5@gmail.com>
2021-06-18 07:47:52 -0700

    alphabelts

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 char ch;
-for (ch = 'a' ; ch = 'z' ; ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 putchar(ch);
 }
-for (ch = 'A' ; ch = 'Z' ; ch++)
+for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
 putchar(ch);
 }

d8c43fe4602721247df27dee9e254526f81e18bf
root <oyeyemikorede5@gmail.com>
2021-06-18 08:00:34 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - print alphabelts execpt e and q
+*
+* Description: using the main function
+* this program prints "a-z execpt e and q"
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ' ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}

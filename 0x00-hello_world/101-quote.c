aa47f08f2fe958c3755af736602e8f6ed9ca6307
root <oyeyemikorede5@gmail.com>
2021-06-20 11:17:26 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 /**
 * main - Entry point
-* 
-* Description: using the main function 
+* Description: using the main function
 * prints "and the piece of art is useful"
 * Return: 0
 */
 int main(void)
-{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }

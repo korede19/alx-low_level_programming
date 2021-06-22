1c8e59b8eccb0c8993822eb8893efe1c5bf5e754
root <oyeyemikorede5@gmail.com>
2021-06-22 02:44:24 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* main - print abc 10 times
+*
+* Description: function to print abc 10 times
+* print "abc.."
+* Return: 0
+*/
+void print_alphabet_x10(void)
+{
+char c, i;
+for (i = 0; i <= 9; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}

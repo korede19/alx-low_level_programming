5a28804901d0adabf528ae1fe1e14e692f127af5
root <oyeyemikorede5@gmail.com>
2021-06-22 04:24:42 -0700

    print holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-char hoberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 unsigned int c;
 for (c = 0; c < sizeof(holberton); c++)
 {

401bec3597d6ea29dd7d6d05bf385019fc283415
root <oyeyemikorede5@gmail.com>
2021-06-23 09:04:37 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+* times_table - discription around the corner
+* Return: 0
+*/
+void times_table(void)
+{
+int row, column, answer;
+for (row = 0; row <= 9; row++)
+{
+for (column = 0; column <= 9; column++)
+{
+answer = row * column;
+if (column != 0)
+{
+_putchar(',');
+_putchar(' ');
+if (answer <= 9)
+{
+_putchar(' ');
+}
+}
+if (answer > 9)
+{
+_putchar(answer / 10 + '0');
+}
+_putchar(answer % 10 + '0');
+}
+_putchar('\n');
+}
+}
+	      

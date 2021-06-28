66d7bac0efa0470f73611fb5a090515cdbf7a6d9
korede <oyeyemikorede5@gmail.com>
2021-06-28 16:13:27 +0100

    holberton file

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -7,5 +7,9 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif

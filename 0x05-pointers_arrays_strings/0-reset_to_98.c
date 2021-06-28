3cafe98035b21efd67ea0ea10c8c3635ffc868aa
korede <oyeyemikorede5@gmail.com>
2021-06-28 13:22:04 +0100

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+* reset_to_98 - check the code for holberton School Students
+* @n: declaration for function to reset_to_98
+* Return: 0
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}

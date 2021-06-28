46e9a252d4e88bd3e92e242137ae709b59428e6a
korede <oyeyemikorede5@gmail.com>
2021-06-28 14:25:31 +0100

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* _puts - function peints a string
+* @str: declaration of *str
+* Return: 0
+*/
+void _puts(char *str)
+{
+int c;
+
+for (c = 0; str[c] != 0; c++
+{
+_putchar(str[c]);
+}
+_putchar('\n');
+}

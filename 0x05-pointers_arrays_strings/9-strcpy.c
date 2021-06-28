4dbef212a62d0d28ba73e19f5d436c24a6589fca
korede <oyeyemikorede5@gmail.com>
2021-06-28 16:49:39 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include "string.h"
+/**
+* *_strcpy - copies the string pointed to by src
+* @dest: character pointer
+* @src: scharacter source
+* Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0 src[i]; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = src[i];
+return (dest);
+}

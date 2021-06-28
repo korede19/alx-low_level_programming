6bd9f0472ae24809a3c08050fc8974e8778b27c1
korede <oyeyemikorede5@gmail.com>
2021-06-28 14:08:16 +0100

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _strlen - returns length of a string
+* @s: declaration of *s
+* Return: 0
+*/
+int _strlen(char *s)
+(
+int c;
+
+for (c = 0; s[c] != 0; c++)
+{
+}
+return (c)
+}

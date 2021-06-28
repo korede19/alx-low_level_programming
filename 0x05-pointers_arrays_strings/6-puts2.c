b120eff07bf4c826ce8f5d32c7ec4513aa183ea4
korede <oyeyemikorede5@gmail.com>
2021-06-28 15:31:03 +0100

    Half the lies they tell about me are not true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton"
+/**
+* puts2 - func that prints 1 char/2 of a string
+* @str: declaration of str
+* Return: 0
+*/
+void puts2(char *str)
+{
+int c;
+char l;
+
+for (c = 0; str[c] != 0; c++)
+{
+if (c % 2 == 0)
+{
+l = str[c];
+_putchar(l);
+}
+}
+_putchar('/n');
+}

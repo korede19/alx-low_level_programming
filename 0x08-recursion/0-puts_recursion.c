678c08e958786043ac6ddf232196d61b4ad9e939
Oyeyemi Korede <oyeyemikorede5@gmail.com>
2021-07-07 20:31:54 +0100

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* _puts_recursion - function that prints a string, followed by a new line
+* @s: string to print recursively
+* Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+if (s[0] == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(s[0]);
+_puts_recursion(&s[1]);
+}

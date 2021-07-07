b69d0fd3b0946330701522a7bcccd03c13630818
Oyeyemi Korede <oyeyemikorede5@gmail.com>
2021-07-07 20:30:10 +0100

    holberton file

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,16 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+void _puts_recursion(char *s);
+void _putchar(int c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int calculate_sqrt(int n, int i);
+int calculate_prime(int n, int i);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+
+#endif /* _HOLBERTON_H_ */

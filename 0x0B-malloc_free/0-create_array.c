bbac90cebdbc330a9113b14f76014b4f4d4770fd
Oyeyemi Korede <oyeyemikorede5@gmail.com>
2021-07-13 13:23:39 +0100

    0. Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*create_array - main function
+*@size : the size of the integer
+*@c : the car that will have the size
+*Return: character
+*/
+
+char *create_array(unsigned int size, char c)
+{
+char *s;
+if (size == 0)
+{
+return (NULL);
+}
+s = malloc(sizeof(char) * size);
+if (s == NULL)
+{
+return (NULL);
+}
+else
+{
+unsigned int i = 0;
+while (s[i] == '\0')
+{
+s[i] = c;
+i++;
+}
+}
+return (s);
+}

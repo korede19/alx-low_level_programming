ecc6ba603c8ee27235ae45a26b3b550d09c3ceb3
unknown <oyeyemikorede5@gmail.com>
2021-07-20 12:36:52 +0100

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+* print_name - use the function passed to it to print the print_name
+* @name : The name we want to print
+* @f : the function that will print the print
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+if (f != NULL)
+f(name);
+}

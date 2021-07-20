4b41832c1d0ae2c9c3b8fdc0d4025a1250317415
unknown <oyeyemikorede5@gmail.com>
2021-07-20 12:38:57 +0100

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - use the function passed to do an action
+* @array : The array that has the value
+* @size : the size of the array
+* @action: The function that does something
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (array != NULL && action != NULL)
+{
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
+}

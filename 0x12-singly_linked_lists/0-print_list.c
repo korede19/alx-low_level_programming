ea5f69f9b185f5696dad3fe73d77c7cf242fe15f
unknown <oyeyemikorede5@gmail.com>
2021-07-29 15:28:12 +0100

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+* print_list - a function that print all strings from a structure
+* @h : the head of the struct
+* Return: the length of the string
+*/
+
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+while (h)
+{
+if (h->str != NULL)
+printf("[%u] %s\n", h->len, h->str);
+else
+printf("[0] (nil)\n");
+h = h->next;
+i++;
+}
+return (i);
+}

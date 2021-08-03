b7649c20a675191995b8682db24bf27bb643619f
unknown <oyeyemikorede5@gmail.com>
2021-08-03 10:13:56 +0100

    6. Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+* pop_listint - remove the head of a linked list
+* @head : the head of a linked list
+* Return: the value in the list
+*/
+
+int pop_listint(listint_t **head)
+{
+int n;
+listint_t *t;
+
+if (!*head)
+return (0);
+else
+{
+t = *head;
+*head = (*head)->next;
+n = (t)->n;
+free(t);
+}
+return (n);
+}

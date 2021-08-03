fa7edadd4003a71ca1d307d0aa52f525fab66884
unknown <oyeyemikorede5@gmail.com>
2021-08-03 10:38:05 +0100

    12. Print (safe version)

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* print_listint_safe - prints a list without going through an infinite loop
+* @head: beginning of linked list
+*
+* Return: size of list
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+size_t nodes = 1;
+if (head == NULL)
+return (0);
+/* act = head->next;
+printf("[%p] %d\n", (void *)&act, head->n);
+nodes += print_listint_safe(head->next);*/
+return (nodes);
+}

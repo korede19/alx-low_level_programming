b1a649586ac1a510228410f3be612744fba967d6
unknown <oyeyemikorede5@gmail.com>
2021-08-03 10:10:58 +0100

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* free_listint2 - The function that free a memory location
+* @head: the head of the linked list
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *tm;
+if (!head)
+return;
+while (*head)
+{
+tm = (*head)->next;
+free(*head);
+*head = tm;
+}
+free(tm);
+free(*head);
+}

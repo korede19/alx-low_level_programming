d7fa0a349793f9adf406872a52c37d2295a35875
unknown <oyeyemikorede5@gmail.com>
2021-08-03 10:19:42 +0100

    7. Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,12 +1,10 @@
 #include "lists.h"
-
 /**
 * get_nodeint_at_index - function that return an index of node
 * @head: the head of the linked list
 * @index: the index we want to remove
 * Return: the node from the index
 */
-
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i = 0;

d63e7ab5b1aa808051413d0f10dfe1fe85d07f01
unknown <oyeyemikorede5@gmail.com>
2021-08-05 09:51:15 +0100

    file

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+#endif
